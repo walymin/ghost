fe2874c380f6abf59819e6668ae4cda80887ff74
walymin <287525101@qq.com>
2016-07-19 20:51:14 +0800

    Update config.example.js

--- config.example.js
+++ config.example.js
@@ -11,7 +11,7 @@ config = {
     // When running Ghost in the wild, use the production environment.
     // Configure your URL and mail settings here
     production: {
-        url: 'http://bigming.daoapp.io',
+        url: 'http://calcifer.daoapp.io',
         mail: {},
         database: {
             client: 'mysql',
